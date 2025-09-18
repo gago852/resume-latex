51a1c76b4b4a497a79075b475ac4e04f2d6165ca
Gabriel Gomez <ggomez.dev@outlook.com>
2025-09-18 14:50:59 -0500

    feat: add file parameter to build scripts for flexible resume compilation
    Signed-off-by: Gabriel Gomez <ggomez.dev@outlook.com>

--- build.ps1
+++ build.ps1
@@ -1,5 +1,9 @@
+param(
+    [string]$File = 'master_resume_en.tex'
+)
+
 # Build the Docker image
 docker build -t latex .
 
 # Run the Docker container
-docker run --rm -i -v "${PWD}:/data" latex pdflatex gabriel_gomez_resume_es.tex
+docker run --rm -i -v "${PWD}:/data" latex pdflatex $File
--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+# Usage:
+#   ./build.sh                 # builds default resume file
+#   ./build.sh <file.tex>      # builds the specified LaTeX file
+
+TARGET_FILE="${1:-master_resume_en.tex}"
+
 docker build -t gago852/latex .
-docker run --rm -i -v "$PWD":/data gago852/latex pdflatex gabriel_gomez_resume.tex
+docker run --rm -i -v "$PWD":/data gago852/latex pdflatex "$TARGET_FILE"
